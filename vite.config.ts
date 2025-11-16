901e44d431b0d3ed54dadef9221152d781bdaa46
Admin5152 <sethagyeimensah2@gmail.com>
2025-11-16 13:17:37 +0000

    Configure for GitHub Pages deployment2

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Portfolio-Site/" : "/",
-  server: {
+    base: "/Portfolio-Site/",   // <-- MUST MATCH EXACT REPO NAME EXACTLY
+    server: {
     host: "::",
     port: 8080,
   },
